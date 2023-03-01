e3a9f38f1565051c0e7008b2c2af99e392e0ac84
fanyou <youfan.noey@gmail.com>
2023-02-28 19:11:53 -0800

    Update scripts

--- scripts/fetcher.py
+++ scripts/fetcher.py
@@ -6,7 +6,7 @@ import requests
 beds = 3
 price = 2500000
 pending = 'false'
-url = "https://www.redfin.com/stingray/api/gis-csv?al=3&include_pending_homes=false&isRentals=false&market=seattle&max_price={price}&num_beds={beds}&num_homes=10000&ord=last-sale-date-asc&page_number=1&region_id={region}&region_type={type}&sold_within_days=1825&status=1&uipt=1&v=8"
+url = "https://www.redfin.com/stingray/api/gis-csv?al=1&include_pending_homes=false&isRentals=false&market=seattle&max_price={price}&num_beds={beds}&num_homes=10000&ord=last-sale-date-asc&page_number=1&region_id={region}&region_type={type}&sold_within_days=1825&status=1&uipt=1&v=8"
 
 with open('./regions.json') as rf:
     regions = json.load(rf)
@@ -18,11 +18,11 @@ with open('./regions.json') as rf:
         downloadCmd = 'curl \'{url}\' > {output}'.format(url=regionUrl, output=output);
 
         # Show the url
-        # print(regionUrl)
+        print(regionUrl)
 
         # delegate to shell to bypass recaptcha check
         # time.sleep(1)
         os.system(openCmd)
-        time.sleep(3)
+        time.sleep(30)
         os.system(downloadCmd)
 
